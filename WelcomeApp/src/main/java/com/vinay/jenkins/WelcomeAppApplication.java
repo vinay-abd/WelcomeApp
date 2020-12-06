17b83738a90fd2215d7a94202fcc347b6a45455e
USIT11 <USIT11@DESKTOP-0F25Q69>
2020-12-06 13:28:00 +0530

    Added new project.

---
+++ WelcomeApp/src/main/java/com/vinay/jenkins/WelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeAppApplication.class, args);
+	}
+
+}
