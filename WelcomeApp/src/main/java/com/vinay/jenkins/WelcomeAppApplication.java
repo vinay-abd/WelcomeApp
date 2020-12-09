f5bd6e1d3a1e2556127f9f033087450ad62e9469
USIT11 <USIT11@DESKTOP-0F25Q69>
2020-12-09 19:18:28 +0530

    ok

--- WelcomeApp/src/main/java/com/vinay/jenkins/WelcomeAppApplication.java
+++ WelcomeApp/src/main/java/com/vinay/jenkins/WelcomeAppApplication.java
@@ -1,10 +1,18 @@
 package com.vinay.jenkins;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.ServletContextApplicationContextInitializer;
 
 @SpringBootApplication
-public class WelcomeAppApplication {
+public class WelcomeAppApplication extends ServletContextApplicationContextInitializer{
+
+	public WelcomeAppApplication(ServletContext servletContext) {
+		super(servletContext);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WelcomeAppApplication.class, args);
