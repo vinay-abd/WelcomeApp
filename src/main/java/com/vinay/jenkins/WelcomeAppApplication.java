b35c9a9ee2a88b1e74b2f6de4cce0b248b28b217
vinay-abd <karchi.vinay@gmail.com>
2021-05-16 21:11:47 +0530

    Update WelcomeAppApplication.java

--- src/main/java/com/vinay/jenkins/WelcomeAppApplication.java
+++ src/main/java/com/vinay/jenkins/WelcomeAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WelcomeAppApplication extends ServletContextApplicationContextInitializer {
+public class WelcomeAppApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WelcomeAppApplication.class, args);
